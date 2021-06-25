2abb28e18d7e522408d09d10ee18ce2e18ea93b3
Abdullo <abdullokh2020@gmail.com>
2021-06-25 13:59:16 +0600

    new

---
+++ src/main/java/kg/socservice/faceRecognition/FaceRecognitionApplication.java
@@ -0,0 +1,13 @@
+package kg.socservice.faceRecognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceRecognitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceRecognitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/socservice/faceRecognition/FaceRecognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.socservice.faceRecognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceRecognitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
